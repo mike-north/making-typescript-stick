6b8c67c178a759566e13c0901902da5940163e83
Mike North <michael.l.north@gmail.com>
2022-03-21 22:09:46 -0500

    roll back solutions (#4)

--- challenges/async-communicator/src/index.ts
+++ challenges/async-communicator/src/index.ts
@@ -1,11 +1,10 @@
-import { expectType } from 'tsd';
+import { expectType } from "tsd";
 
-export type WrapForPenpal<Methods extends Record<string, (...args: any[]) => any>> = {
-  [K in keyof Methods & string]: (...args: Parameters<Methods[K]>) => Promise<ReturnType<Methods[K]>>;
-}
+// IMPLEMENT THIS TYPE
+export type WrapForPenpal<T> = any;
 
 /**
- * Test Scenario
+ * Test Scenario - Do not change anything below this line
  */
 const methods = {
   add(a: number, b: number): number {
@@ -13,13 +12,16 @@ const methods = {
   },
   subtract(a: number, b: number): number {
     return a - b;
-  }
-}
+  },
+};
 const asyncMethods: WrapForPenpal<typeof methods> = {} as any;
 
 let addPromise = asyncMethods.add(1, 2);
 expectType<Promise<number>>(addPromise);
+// @ts-expect-error
+expectType<typeof addPromise>("this should fail");
 
 let subtractPromise = asyncMethods.subtract(1, 2);
 expectType<Promise<number>>(subtractPromise);
-
+// @ts-expect-error
+expectType<typeof subtractPromise>("this should fail");
