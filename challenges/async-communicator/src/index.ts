7f9fc098b07d7aa4f5d13bd69e84537cfd3abc1a
Mike North <michael.l.north@gmail.com>
2022-03-22 19:25:32 -0500

    penpal exercise solution

--- challenges/async-communicator/src/index.ts
+++ challenges/async-communicator/src/index.ts
@@ -1,14 +1,24 @@
 import { expectType } from "tsd";
 
 // IMPLEMENT THIS TYPE
-export type WrapForPenpal<T> = any;
+export type WrapForPenpal<T> = {
+  [K in keyof T]: T[K] extends { (...arg: infer Args): infer R }
+    ? R extends Promise<any>
+      ? (...arg: Args) => R // R is already a promise
+      : (...arg: Args) => Promise<R> // R needs to be "wrapped"
+    : T[K]; // non-method things
+};
 
 /**
  * Test Scenario - Do not change anything below this line
  */
 const methods = {
+  foo: 6,
   add(a: number, b: number): number {
     return a + b;
+  },
+  doAsyncThing(a: number, b: number): Promise<number> {
+    return Promise.resolve(a + b);
   },
   subtract(a: number, b: number): number {
     return a - b;
@@ -16,6 +26,7 @@ const methods = {
 };
 const asyncMethods: WrapForPenpal<typeof methods> = {} as any;
 
+let addPromise1 = asyncMethods.doAsyncThing(1, 2);
 let addPromise = asyncMethods.add(1, 2);
 expectType<Promise<number>>(addPromise);
 // @ts-expect-error
